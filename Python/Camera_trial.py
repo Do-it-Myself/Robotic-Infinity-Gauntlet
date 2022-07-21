6ca3769729825ef150d4ee9bed8dfb156643a4ac
SweetTomato111 <doitmyself111.engineering@gmail.com>
2022-07-21 21:19:16 +0800

    Update Camera_trial.py

--- Python/Camera_trial.py
+++ Python/Camera_trial.py
@@ -1,1 +1,9 @@
 # https://www.raspberrypi.com/documentation/accessories/camera.html
+# install picamera library: https://picamera.readthedocs.io/en/release-1.13/install.html
+
+from picamera import PiCamera
+from time import sleep
+camera = PiCamera()
+camera.start_preview(alpha=192)
+sleep(1)
+camera.capture("/home/pi/Desktop")
