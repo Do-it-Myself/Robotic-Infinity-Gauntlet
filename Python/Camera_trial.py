e87fef25b4a378d06319bcdeb792ec04dca7bbf3
SweetTomato111 <doitmyself111.engineering@gmail.com>
2022-07-22 23:47:17 +0800

    added stuff

--- Python/Camera_trial.py
+++ Python/Camera_trial.py
@@ -4,6 +4,7 @@
 from picamera import PiCamera
 from time import sleep
 camera = PiCamera()
-camera.start_preview(alpha=192)
-sleep(1)
-camera.capture("/home/pi/Desktop")
+camera.start_preview()
+sleep(5)
+camera.capture("/home/pi/Desktop/image.jpg")
+camera.stop_preview()
