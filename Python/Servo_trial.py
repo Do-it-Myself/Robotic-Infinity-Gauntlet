81a4fdf84e74c740565a96c599681ab7f3527007
SweetTomato111 <doitmyself111.engineering@gmail.com>
2022-07-26 17:12:49 +0800

    Update Servo_trial.py

--- Python/Servo_trial.py
+++ Python/Servo_trial.py
@@ -3,13 +3,21 @@
 from gpiozero import Servo
 from time import sleep
 
-servo1 = Servo(25, min_pulse_width = 0.33/1000)
+servo1 = Servo(23, min_pulse_width = 0.33/1000)
 servo2 = Servo(24, min_pulse_width = 0.33/1000)
-
+servo3 = Servo(25, min_pulse_width = 0.33/1000)
+servo4 = Servo(8, min_pulse_width = 0.33/1000)
+servo5 = Servo(7, min_pulse_width = 0.33/1000)
 while True:
     servo1.value = -1
     servo2.value = -1
+    servo3.value = -1
+    servo4.value = -1
+    servo5.value = -1
     sleep(1)
     servo1.value = 1
     servo2.value = 1
+    servo3.value = 1
+    servo4.value = 1
+    servo5.value = 1
     sleep(2)
